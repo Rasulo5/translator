7b61d18f46867acd4359bc28f9d919781d3a9d9d
Rasulo5 <148695714+Rasulo5@users.noreply.github.com>
2024-07-10 00:48:48 +0300

    Add files via upload

---
+++ precache-manifest.bb4e0e422aba8f51652bc44e24429a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb65636db40bc30a173480b11a3b992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f856f86aac99be5f10f6",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e40ddf2b9b671016ed71",
+    "url": "/static/js/2.ef63edac.chunk.js"
+  },
+  {
+    "revision": "f856f86aac99be5f10f6",
+    "url": "/static/js/main.a1c78bf0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
